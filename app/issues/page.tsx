54c52946589c46001e22734c159f84c228df2615
Creative-Me-Sanket <khule.sanket39396@gmail.com>
2025-03-28 12:41:09 +0530

    radix-ui setup

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,11 @@
 
 import React from 'react'
+import { Button } from '@radix-ui/themes'
 
 const Issues = () => {
   return (
     <div>
-      <h1>Issues</h1>
+      <Button>New Issue</Button>
     </div>
   )
 }
